7e59c0a3556604c5b8ff13dcdcc386016d1005b7
Nguyen Dung <dungnt@vivas.vn>
2017-05-04 10:01:17 +0700

    cm

--- game-server/Package.swift
+++ game-server/Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "game-server"
+    name: "game-server",
+    dependencies: [
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,901)),
+        ]
 )
