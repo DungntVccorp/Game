d30aaa84d65d65596d47d84aa33010c341b2bdf0
Nguyen Tien Minh <dung.nt.a5901679@gmail.com>
2017-05-04 09:55:49 +0700

    cm

---
+++ game-server/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "game-server",
+    targets: [
+        .target(
+            name: "game-server",
+            dependencies: []),
+    ]
+)
---
+++ game-server/README.md
@@ -0,0 +1,3 @@
+# game-server
+
+A description of this package.
