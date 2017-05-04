61cc2eab41c189a72abf079f7743061e40f0f22f
Nguyen Dung <dungnt@vivas.vn>
2017-05-04 10:42:28 +0700

    cm

--- game-server/Package.swift
+++ game-server/Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "game-server",
     dependencies: [
-        .Package(url: "https://github.com/1024jp/GzipSwift.git", Version(3,1,4)),
         .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,901)),
         .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", Version(1,7,0)),
         ]
